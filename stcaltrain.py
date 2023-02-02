04629be7f3d193e8d79f6e22ddc2a28ff88bc631
Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
2023-02-02 06:43:29 -0800

    .

--- stcaltrain.py
+++ stcaltrain.py
@@ -101,6 +101,7 @@ def ping_caltrain(station):
     st.write(arrs)
     # 0 if a diff is negative
     # time_diffs = [i if i.total_seconds() > 0 else datetime.timedelta(0) for i in diffs]
+    time_diffs = [i for i in diffs]
     time_diffs = [str(i)[0:4] for i in time_diffs]
 
     # Add the time difference to the dataframe
