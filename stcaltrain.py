bb315d746a755f78b1dae8d8d8c2d8c7059a9d0d
Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
2023-02-01 22:09:10 -0800

    final line

--- stcaltrain.py
+++ stcaltrain.py
@@ -160,6 +160,9 @@ st.dataframe(caltrain_data_sb)
 col1, col2 = st.columns([4, 3])
 with col1:
     st.markdown("---")
+
+col1, col2 = st.columns([4, 3])
+with col1:
     badge("twitter", "TYLERSlMONS", "https://twitter.com/TYLERSlMONS")
 with col2:
     badge("github", "tyler-simons/caltrain", "https://github.com/tyler-simons/caltrain")
