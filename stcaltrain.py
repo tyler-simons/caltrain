8d16ac457ebf46f9f712e2d09d42f5abe7a76101
Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
2023-12-03 23:49:30 -0800

    Squashed commit of the following:
    commit 13812ee80cfd9be8dd1b20b4e905a0c51fcdd86f
    Author: Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
    Date:   Sun Dec 3 23:48:07 2023 -0800
    
        fix missing data
    
    commit 2d1cf62ebf418d044de1128a06d9b4f227a32e1f
    Author: Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
    Date:   Sun Dec 3 23:35:45 2023 -0800
    
        add secrets to gitignore
    
    commit d80d93a48912e3bab9ecddb1ed4869222a52cf11
    Author: Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
    Date:   Sun Dec 3 23:35:20 2023 -0800
    
        shhhh
    
    commit a2c53aa1cb96b5f70f727cb4efd9373a079a44bb
    Author: Tyler Simons <39609292+tyler-simons@users.noreply.github.com>
    Date:   Sun Dec 3 23:32:28 2023 -0800
    
        replace caltrain live map with 511

--- stcaltrain.py
+++ stcaltrain.py
@@ -13,9 +13,148 @@ from functions.ct_functions import (
     build_caltrain_df,
     create_train_df,
 )
+from geopy.distance import geodesic
+import requests
+import json
 
 st.set_page_config(page_title="Caltrain Platform", page_icon="🚆", layout="centered")
 
+
+@st.cache_resource(ttl="60s")
+def ping_train() -> dict:
+    # URL for the 511 Transit API
+    url = f"https://api.511.org/transit/VehicleMonitoring?api_key={st.secrets['511_key']}&agency=CT"
+
+    # Making the request
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Decode using utf-8-sig to handle UTF-8 BOM
+        decoded_content = response.content.decode("utf-8-sig")
+
+        # Parse the decoded content into JSON
+        data = json.loads(decoded_content)
+
+    else:
+        print("Failed to retrieve data: ", response.status_code)
+
+    if data["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"].get("VehicleActivity") is None:
+        return False
+    else:
+        return data
+
+
+data = ping_train()
+
+
+def create_caltrain_dfs(data: dict) -> pd.DataFrame:
+    """Ping 511 API and reformat the data"""
+    trains = []
+    for train in data["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]["VehicleActivity"]:
+        train_obj = train["MonitoredVehicleJourney"]
+
+        if train_obj.get("OnwardCalls") is None:
+            continue
+
+        destinations_df = pd.DataFrame(
+            [
+                [
+                    stop["StopPointName"],
+                    stop["StopPointRef"],
+                    stop["AimedArrivalTime"],
+                    stop["ExpectedArrivalTime"],
+                ]
+                for stop in train_obj["OnwardCalls"]["OnwardCall"]
+            ],
+            columns=["stop_name", "stop_id", "aimed_arrival_time", "expected_arrival_time"],
+        )
+        destinations_df["id"] = train_obj["VehicleRef"]
+        destinations_df["origin"] = train_obj["OriginName"]
+        destinations_df["origin_id"] = train_obj["OriginRef"]
+        destinations_df["direction"] = train_obj["DirectionRef"] + "B"
+        destinations_df["line_type"] = train_obj["PublishedLineName"]
+        destinations_df["destination"] = train_obj["DestinationName"]
+        destinations_df["train_longitude"] = train_obj["VehicleLocation"]["Longitude"]
+        destinations_df["train_latitude"] = train_obj["VehicleLocation"]["Latitude"]
+
+        destinations_df = destinations_df[
+            [
+                "id",
+                "origin",
+                "origin_id",
+                "direction",
+                "line_type",
+                "destination",
+                "stop_name",
+                "stop_id",
+                "aimed_arrival_time",
+                "expected_arrival_time",
+                "train_longitude",
+                "train_latitude",
+            ]
+        ]
+        destinations_df["stops_away"] = destinations_df.index
+        trains.append(destinations_df)
+    trains_df = pd.concat(trains)
+
+    # Change to the correct types
+    trains_df["aimed_arrival_time"] = pd.to_datetime(trains_df["aimed_arrival_time"])
+    trains_df["expected_arrival_time"] = pd.to_datetime(trains_df["expected_arrival_time"])
+    trains_df["train_longitude"] = trains_df["train_longitude"].astype(float)
+    trains_df["train_latitude"] = trains_df["train_latitude"].astype(float)
+    trains_df["stop_id"] = trains_df["stop_id"].astype(float)
+    trains_df["origin_id"] = trains_df["origin_id"].astype(float)
+
+    # Import the stop_ids and add their coordinates to the dataframe
+    stop_ids = pd.read_csv("stop_ids.csv")
+
+    # Combine the stop IDs
+    sb_trains_df = pd.merge(trains_df, stop_ids, left_on="stop_id", right_on="stop1", how="inner")
+    nb_trains_df = pd.merge(trains_df, stop_ids, left_on="stop_id", right_on="stop2", how="inner")
+    trains_df = pd.concat([sb_trains_df, nb_trains_df])
+
+    trains_df["distance"] = trains_df.apply(
+        lambda x: geodesic((x["train_latitude"], x["train_longitude"]), (x["lat"], x["lon"])).miles,
+        axis=1,
+    )
+    trains_df["distance"] = trains_df["distance"].round(1).astype("str") + " mi"
+    trains_df["Departure Time"] = trains_df["expected_arrival_time"]
+    trains_df["Current Time"] = datetime.datetime.now(pytz.timezone("UTC"))
+    trains_df["ETA"] = trains_df["Departure Time"] - trains_df["Current Time"]
+    trains_df["Train #"] = trains_df["id"]
+    trains_df["Direction"] = trains_df["direction"]
+
+    return trains_df
+
+
+def clean_up_df(data: pd.DataFrame) -> pd.DataFrame:
+    """Clean up the dataframe for display"""
+    # Filter for desired columns
+    data = data[["Train #", "Train Type", "Departure Time", "ETA", "distance", "stops_away"]]
+    data["ETA"] = data["ETA"].astype(str).str[7:12]
+
+    # Rename the columns
+    data.columns = [
+        "Train #",
+        "Train Type",
+        "Departure Time",
+        "ETA",
+        "Distance to Station",
+        "Stops Away",
+    ]
+
+    data = data.T
+    data.columns = data.iloc[0]
+    data = data.drop(data.index[0])
+
+    return data
+
+
+if data is not False:
+    caltrain_data = create_caltrain_dfs(data)
+else:
+    caltrain_data = False
 
 st.title("🚊 Caltrain Platform 🚂")
 caltrain_stations = pd.read_csv("stop_ids.csv")
@@ -32,7 +171,6 @@ chosen_station = col1.selectbox("Choose Origin Station", caltrain_stations["stop
 chosen_destination = col1.selectbox(
     "Choose Destination Station", ["--"] + caltrain_stations["stopname"].tolist(), index=0
 )
-caltrain_data = ping_caltrain(chosen_station, destination=chosen_destination)
 api_working = True if type(caltrain_data) == pd.DataFrame else False
 scheduled = False
 
@@ -80,45 +218,71 @@ if display == "Scheduled":
             ]
         )
 
+    # Sort by ETA
+    caltrain_data = caltrain_data.sort_values(by=["ETA"])
+    caltrain_data_nb = caltrain_data.query("Direction == 'NB'").drop("Direction", axis=1)
+    caltrain_data_sb = (
+        caltrain_data.query("Direction == 'SB'").drop("Direction", axis=1).reset_index(drop=True)
+    )
+
+    # Reset the index to 1, 2, 3.
+    caltrain_data_nb.index = caltrain_data_nb.index + 1
+    caltrain_data_sb.index = caltrain_data_sb.index + 1
+
+    col1, col2 = st.columns([2, 1])
+
+    # Display the dataframes split by Train #, Scheduled Departure, Current Stop and the other columns
+    col1.subheader("Northbound Trains")
+    nb_data = caltrain_data_nb.T
+    nb_data.columns = nb_data.iloc[0]
+    nb_data = nb_data.drop(nb_data.index[0])
+    col1.dataframe(nb_data, use_container_width=True)
+
+    col1.subheader("Southbound Trains")
+    sb_data = caltrain_data_sb.T
+    sb_data.columns = sb_data.iloc[0]
+    sb_data = sb_data.drop(sb_data.index[0])
+    col1.dataframe(sb_data, use_container_width=True)
+
 else:
     col1.success("✅ Caltrain Live Map API is up and running.")
-caltrain_data["Train Type"] = caltrain_data["Train #"].apply(lambda x: assign_train_type(x))
 
-# Localize to Pacific Time
-if scheduled == False:
+    caltrain_data["Train Type"] = caltrain_data["Train #"].apply(lambda x: assign_train_type(x))
+
     caltrain_data["Departure Time"] = (
         pd.to_datetime(caltrain_data["Departure Time"])
-        .dt.tz_localize("UTC")
         .dt.tz_convert("US/Pacific")
         .dt.strftime("%I:%M %p")
     )
 
-# Split the caltrain data based on direction and drop the direction column
-# sort by eta
-caltrain_data = caltrain_data.sort_values(by=["ETA"])
-caltrain_data_nb = caltrain_data.query("Direction == 'NB'").drop("Direction", axis=1)
-caltrain_data_sb = (
-    caltrain_data.query("Direction == 'SB'").drop("Direction", axis=1).reset_index(drop=True)
-)
+    # Filter for destinations
+    if chosen_destination != "--":
+        destinations = caltrain_data[caltrain_data["destination"] == chosen_destination]["id"]
+        caltrain_data = caltrain_data[caltrain_data["id"].isin(destinations)]
 
-# Reset the index to 1, 2, 3.
-caltrain_data_nb.index = caltrain_data_nb.index + 1
-caltrain_data_sb.index = caltrain_data_sb.index + 1
+    # # Display the dataframes split by Train #, Scheduled Departure, Current Stop and the other columns
 
-col1, col2 = st.columns([2, 1])
+    # Northbound Trains
+    col1.subheader("Northbound Trains")
+    nb_trains = caltrain_data.query("Direction == 'NB'").drop("Direction", axis=1)
+    nb_trains = nb_trains.sort_values(by=["ETA"])
+    nb_trains = nb_trains[nb_trains["stopname"] == chosen_station]
 
-# Display the dataframes split by Train #, Scheduled Departure, Current Stop and the other columns
-col1.subheader("Northbound Trains")
-nb_data = caltrain_data_nb.T
-nb_data.columns = nb_data.iloc[0]
-nb_data = nb_data.drop(nb_data.index[0])
-col1.dataframe(nb_data, use_container_width=True)
+    if nb_trains.empty:
+        col1.info("No trains")
+    else:
+        col1.dataframe(clean_up_df(nb_trains), use_container_width=True)
 
-col1.subheader("Southbound Trains")
-sb_data = caltrain_data_sb.T
-sb_data.columns = sb_data.iloc[0]
-sb_data = sb_data.drop(sb_data.index[0])
-col1.dataframe(sb_data, use_container_width=True)
+    # Southbound trains
+    col1.subheader("Southbound Trains")
+    sb_data = caltrain_data.query("Direction == 'SB'").drop("Direction", axis=1)
+    sb_data = sb_data.sort_values(by=["ETA"])
+    sb_data = sb_data[sb_data["stopname"] == chosen_station]
+
+    if len(sb_data) == 0:
+        col1.info("No trains")
+    else:
+        col1.dataframe(clean_up_df(sb_data), use_container_width=True)
 
 if col1.button("Refresh Data"):
     st.experimental_rerun()
